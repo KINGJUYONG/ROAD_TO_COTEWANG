13db60149fd6053b474bff136f93924fe6f9d8bd
JuYong Shin <hatake0901@icloud.com>
2023-06-05 15:07:21 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,3 @@
+KI, QU, LO, VI, KN, PA = input().split()
+
+print(str(1 - int(KI)) + " " + str(1 - int(QU))  + " " + str(2 - int(LO)) + " " + str(2 - int(VI)) + " " + str(2 - int(KN)) + " " + str(8 - int(PA)))
