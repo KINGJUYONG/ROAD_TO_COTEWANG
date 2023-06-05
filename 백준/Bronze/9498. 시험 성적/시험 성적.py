57dcbd4c4ecfc1329e60b0be492ce65c8fc1c204
JuYong Shin <hatake0901@icloud.com>
2023-06-05 15:09:48 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = input()
+a = int(a)
+if a > 89 and a < 101:
+    print("A")
+elif a < 90 and a > 79:
+    print("B")
+elif a < 80 and a > 69:
+    print("C")
+elif a < 70 and a > 59:
+    print("D")
+else:
+    print("F")
