abb6a14927a06776be1b72c23924510ae43dc34c
JuYong Shin <hatake0901@icloud.com>
2023-06-05 15:14:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+arr = [0, 1, 2]
+
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    for i in range(3, n+1):
+        arr.append(arr[i-1] + arr[i-2])
+    print(arr[n] % 10007)
